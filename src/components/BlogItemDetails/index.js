3e1436ed1fc97a411ee41f02a2cab7733dca2dfd
Venkatesh2001v <venkateshvenkat2001v@gmail.com>
2023-05-21 00:33:42 +0000

    solution

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,66 @@
+// Write your JS code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}, isLoading: true}
+
+  componentDidMount() {
+    this.getBlogItemDetails()
+  }
+
+  getBlogItemDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+
+    const updatedData = {
+      author: data.author,
+      avatarUrl: data.avatar_url,
+      imageUrl: data.image_url,
+      title: data.title,
+      topic: data.topic,
+      content: data.content,
+    }
+
+    this.setState({blogData: updatedData, isLoading: false})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, avatarUrl, author, imageUrl, content} = blogData
+    return (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="blog-author-name">{author}</p>
+        </div>
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="blog-container">
+        {isLoading ? (
+          <div data-testId="loader" className="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : (
+          this.renderBlogItemDetails()
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogItemDetails
